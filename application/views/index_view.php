6ab10c3b98707b17487d19a6f131fc682cf1df80
Uğur Burak TENDÜRÜS <utendurus@gmail.com>
2013-11-08 22:27:10 +0200

    Project Started now.

---
+++ application/views/index_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CodeIgniter'A Hoşgeldiniz</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>MERHABA</h1>
+	</div>
+</body>
+</html>
